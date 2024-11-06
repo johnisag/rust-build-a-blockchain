a3dd6aa5151fb89e568455a70361aaed5487236b
ioannis <agorgianitisj@hotmail.com>
2024-11-06 20:31:11 +0000

    executing blocks

--- rust-state-machine/src/main.rs
+++ rust-state-machine/src/main.rs
@@ -2,6 +2,8 @@ mod balances;
 mod system;
 mod support;
 
+use crate::support::Dispatch;
+
 // These are the concrete types we will use in our simple state machine.
 // Modules are configured for these types directly, and they satisfy all of our
 // trait requirements.
@@ -50,40 +52,79 @@ impl Runtime {
             balances: balances::Pallet::new(),
         }
     }
+
+	// Execute a block of extrinsics. Increments the block number.
+	fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
+        // Increment the system's block number.
+        self.system.inc_block_number();
+
+        // Check that the block number of the incoming block matches the current block number,
+        // or return an error.
+        if block.header.block_number != self.system.block_number() {
+            return Err("Block number does not match current block number.");
+        }
+
+        // Iterate over the extrinsics in the block...
+        for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
+            // Increment the nonce of the caller.
+            self.system.inc_nonce(&caller);
+
+            // Dispatch the extrinsic using the `caller` and the `call` contained in the extrinsic.
+            // Handle errors from `dispatch` same as we did for individual calls: printing any
+            // error and capturing the result.
+            let _ = self.dispatch(caller, call).map_err(|e| 
+                eprintln!(
+                    "Extrinsic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
+                    block.header.block_number, i, e
+                )
+            );
+        }
+
+        // Return `Ok(())` if everything is successful.
+		Ok(())
+	}
+}
+
+//also ADD THIS CODE TO YOUR main.rs file:
+impl crate::support::Dispatch for Runtime {
+	type Caller = <Runtime as system::Config>::AccountId;
+	type Call = RuntimeCall;
+	// Dispatch a call on behalf of a caller. Increments the caller's nonce.
+	//
+	// Dispatch allows us to identify which underlying module call we want to execute.
+	// Note that we extract the `caller` from the extrinsic, and use that information
+	// to determine who we are executing the call on behalf of.
+	fn dispatch(
+		&mut self,
+		caller: Self::Caller,
+		runtime_call: Self::Call,
+	) -> support::DispatchResult {
+		unimplemented!();
+	}
 }
 
 fn main() {
-    let alice = "alice".to_string();
-    let bob = "bob".to_string();
-    let carlie = "carlie".to_string();
+	let mut runtime = Runtime::new();
+	let alice = "alice".to_string();
+	let bob = "bob".to_string();
+	let charlie = "charlie".to_string();
 
-	// Create a mutable variable `runtime`, which is a new instance of `Runtime`.
-    let mut runtime = Runtime::new();
-
-	//Set the balance of `alice` to 100, allowing us to execute other transactions. 
-    runtime.balances.set_balance(&alice, 100);
+	runtime.balances.set_balance(&alice, 100);
 
 	// start emulating a block
 	runtime.system.inc_block_number();
-
-    assert_eq!(runtime.system.block_number(), 1);
+	assert_eq!(runtime.system.block_number(), 1);
 
 	// first transaction
-    runtime.system.inc_nonce(&alice);
-    let _res = runtime.balances
-    .transfer(alice.clone(), bob.clone(), 30)
-    .map_err(|e| println!("{}", e));
+	runtime.system.inc_nonce(&alice);
+	let _res = runtime
+		.balances
+		.transfer(alice.clone(), bob, 30)
+		.map_err(|e| eprintln!("{}", e));
 
 	// second transaction
-    runtime.system.inc_nonce(&alice);
-    let _res = runtime
-    .balances
-    .transfer(alice.clone(), carlie.clone(), 20)
-    .map_err(|e| println!("{}", e));
+	runtime.system.inc_nonce(&alice);
+	let _res = runtime.balances.transfer(alice, charlie, 20).map_err(|e| eprintln!("{}", e));
 
-    print!("Alice's balance: {}\n", runtime.balances.balance(&alice));
-    print!("Bob's balance: {}\n", runtime.balances.balance(&bob));
-    print!("Carlie's balance: {}\n", runtime.balances.balance(&carlie));
-
-    println!("{:#?}", runtime);
+	println!("{:#?}", runtime);
 }
